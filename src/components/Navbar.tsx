1d514a63ec3bc3f1bd4715fe5c59d9cb6b8d14b4
egamovj <egamovj90@gmail.com>
2024-05-10 12:51:41 +0500

    navbar section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,40 @@ const Navbar = () => {
                 </Link>
               </>
             ) : (
-              <></>
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign up
+                </Link>
+
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: 'ghost'
+                  })}
+                >
+                  Login
+                </Link>
+
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
+              </>
             )}
           </div>
         </div>
