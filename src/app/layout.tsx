df511b1deafc760df32e3236ab5fad8891166318
egamovj <egamovj90@gmail.com>
2024-05-11 11:50:08 +0500

    upload area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,16 +20,19 @@ export default function RootLayout({
 }>) {
   return (
     <>
-    <Head>
-      <link rel="icon" href="/thumbnail.png"/>
-    </Head>
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+      <Head>
+        <link rel="icon" href="/thumbnail.png" />
+      </Head>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+
+          <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+            <div className="flex-1 flex flex-col h-full">{children}</div>
+            <Footer />
+          </main>
+        </body>
+      </html>
     </>
   );
 }
